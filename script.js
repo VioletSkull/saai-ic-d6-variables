cf273ff4568e9a242a802ed6907f3bf5d959db95
VioletSkull <ad3364662@gmail.com>
2024-11-08 04:16:14 +0000

    added javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+let animal = "Red Crested Gecko"
+
+console.log('hello javascript!')
+console.log("I'm writing a 2nd line of JS... awesome")
+console.log(animal) //error - no def for animal variable
+console.log("still here.")
+
+//String
+let personName = "Alex"
+console.log(personName)
+
+//Number
+
+let purchasePrice = 14.79
+console.log("initial value purchasePrice", purchasePrice)
+
+purchasePrice = 9.99
+console.log("updated value purchasePrice")
+
+//Boolean
+let isTuesday = false
+console.log(isTuesday)
